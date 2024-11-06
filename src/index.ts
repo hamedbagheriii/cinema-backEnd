8535f0cea0edf32acac5345869fdba8bef54cdd3
hamed bagheri <hamedbagheri1386.19@gmail.com>
2024-11-06 17:01:04 +0330

    PART2-onBeforeHandle

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,49 @@
-import { Elysia } from "elysia";
+import swagger from "@elysiajs/swagger";
+import { Elysia, error, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(swagger())
 
+  // ! کلی
+  .onBeforeHandle(({ body , path , error}) => {
+    const typedBody = body as { name : string }; 
+
+    if (typedBody?.name.length === 0 && path !== "/") {
+      return error(400, "name is required");
+    }
+  })
+
+  .post(
+    "/data",
+    () => {
+      return {
+        message: "Hello data",
+      };
+    },
+    {   
+      // ! در یک روت
+      // beforeHandle : ({ body , path , error}) => {
+      //   if (path !== "/" && body.name.length === 0) {
+      //     return error(400, "name is required");
+      //   }
+      // },  
+      body: t.Object({
+        name: t.String(),
+      }),
+    }
+  )
+  .post(
+    "/popo",
+    () => {
+      return {
+        message: "Hello popo",
+      };
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+      }),
+    }
+  )
+
+  .listen(3100);
