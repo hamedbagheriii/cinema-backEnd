d1f8f7f1c34fb16128534fcd26d02ce01b7065b4
hamed bagheri <hamedbagheri1386.19@gmail.com>
2024-12-01 02:06:20 +0330

    PART32-editRoles

--- src/roles/role.ts
+++ src/roles/role.ts
@@ -1,94 +1,198 @@
-import Elysia, { t } from "elysia";
-import { Prisma } from "../auth/auth";
+import Elysia, { t } from 'elysia';
+import { Prisma } from '../auth/auth';
 
+export const role = new Elysia().group('/roles', (app) => {
+  return (
+    app
 
-export const role = new Elysia().group("/roles",(app)=>{
-    return app 
-    
-    // ! add new permission (user or admin dont have access) =>
-    .post('/perm',async ({body : {permName}})=>{
-        const newPerm = await Prisma.permission.create({
-            data : {
-                permName
-            }
-        })
-        
-        return {
-            message : 'دسترسی اضافه شد !',
-            success : true,
-            perm : newPerm
-        }
-    },{
-        body : t.Object({
-            permName : t.String()
-        })
-    })
+      // ! ==================== permissions ====================
 
-    // ! add new role =>
-    .post('/add' , async ({body : {roleName , permissions}})=>{
-        const newRole = await Prisma.role.create({
-            data : {
-                roleName,
-                permissions : {
-                    create : permissions.map(permID=>{
-                        return {
-                            permissionID : permID
-                        }
-                    })
-                }
+      // ! add new permission (user or admin dont have access) =>
+      .post(
+        '/perm',
+        async ({ body: { permName, category } }) => {
+          const newPerm = await Prisma.permission.create({
+            data: {
+              permName,
+              category,
             },
-            include : {
-                permissions : {
-                    include : {
-                        permissionData : true
-                    }
-                }
-            }
-        })
+          });
 
-        return  {
-            message : 'نقش با موفقیت اضافه شد !',
-            success : true,
-            role : newRole
-        }
-    },{
-        beforeHandle : async ({body : {roleName , permissions} , set})=>{
-            const checkRolle = await Prisma.role.findUnique({
-                where : {
-                    roleName
-                }
-            })
-
-            // handle check there is permission =>
-            permissions.map( async (permID) => {
-                const checkPerm = await Prisma.permission.findUnique({
-                    where : {
-                        id : permID
-                    }
-                })
-
-                
-                if (checkPerm === null) {
-                    set.status = 404 ;
-                    return {
-                        message : 'مجوز انتخاب شده وجود ندارد !',
-                        success : false
-                    }
-                }
-            })
-
-            // handle check there is role =>
-            if(checkRolle) {
-                return {
-                    message : 'این نقش قبلا ایجاد شده است !',
-                    success : false
-                }
-            }
+          return {
+            message: 'دسترسی اضافه شد !',
+            success: true,
+            perm: newPerm,
+          };
         },
-        body : t.Object({
-            roleName : t.String(),
-            permissions : t.Array(t.Number())
-        })
-    })
+        {
+          body: t.Object({
+            permName: t.String(),
+            category: t.String(),
+          }),
+        }
+      )
 
-})
+      // ! get all perm for show
+      .get('/perm', async () => {
+        const allPerm = await Prisma.permission.findMany();
+
+        return {
+          message: 'دسترسی ها با موفقیت دریافت شد !',
+          success: true,
+          perm: allPerm,
+        };
+      })
+
+      // ! ==================== roles ====================
+
+      //! handle check there is role and there is permission =>
+      .onBeforeHandle(async ({ body, set }) => {
+        const { roleName, permissions } = body as {
+          roleName: string;
+          permissions: number[];
+        };
+        let isPerm: boolean = true;
+
+        // handle check permission =>
+        await Promise.all(
+          permissions.map(async (permID) => {
+            const checkPerm = await Prisma.permission.findUnique({
+              where: {
+                id: permID,
+              },
+            });
+
+            if (checkPerm === null) isPerm = false;
+          })
+        );
+        if (!isPerm) {
+          set.status = 404;
+          return {
+            message: 'مجوز انتخاب شده وجود ندارد !',
+            success: false,
+          };
+        }
+      })
+      .guard({
+        body: t.Object({
+          permissions: t.Array(t.Number()),
+          roleName: t.String(),
+          roleDec: t.String(),
+        }),
+      })
+
+      // ! add new role =>
+      .post(
+        '/add',
+        async ({ body: { roleName, permissions, roleDec } }) => {
+          const newRole = await Prisma.role.create({
+            data: {
+              roleName,
+              roleDec,
+              permissions: {
+                create: permissions.map((permID) => {
+                  return {
+                    permissionID: permID,
+                  };
+                }),
+              },
+            },
+            include: {
+              permissions: {
+                include: {
+                  permissionData: true,
+                },
+              },
+            },
+          });
+
+          return {
+            message: 'نقش با موفقیت اضافه شد !',
+            success: true,
+            role: newRole,
+          };
+        },
+        {
+          beforeHandle: async ({ body: { roleName }, set }) => {
+            const checkRolle = await Prisma.role.findUnique({
+              where: {
+                roleName,
+              },
+            });
+            if (checkRolle) {
+              return {
+                message: 'این نقش قبلا ایجاد شده است !',
+                success: false,
+              };
+            }
+          },
+        }
+      )
+
+      // ! edit role =>
+      .put(
+        '/edit/:id',
+        async ({ body: { roleName, permissions, roleDec }, params: { id } }) => {
+          // handle check there is permission in edit role =>
+          let allPerm: any[] = await Prisma.rolepermissions.findMany({
+            where: {
+              roleID: id,
+            },
+          });
+          allPerm = allPerm.map((t) => t.permissionID);
+
+          const notExist: number[] = allPerm.filter((t) => !permissions.includes(t));
+          const isExist: number[] = permissions.filter((t) => !allPerm.includes(t));
+
+          // remove old and add new permission =>
+          const removePermRole = await Prisma.rolepermissions.deleteMany({
+            where: {
+              roleID: id,
+              permissionID: {
+                in: notExist,
+              },
+            },
+          });
+          const addNewPerm = await Prisma.rolepermissions.createMany({
+            data: isExist.map((permID) => {
+              return {
+                roleID: id,
+                permissionID: permID,
+              };
+            }),
+          });
+
+          // edit role info =>
+          const editRole = await Prisma.role.update({
+            where: {
+              id,
+            },
+            data: {
+              roleName,
+              roleDec,
+            },
+            include: {
+              permissions: {
+                include: {
+                  permissionData: true,
+                },
+              },
+            },
+          });
+
+          return {
+            message: 'نقش با موفقیت ویرایش شد !',
+            success: true,
+            role: editRole,
+          };
+        },
+        {
+          params: t.Object({
+            id: t.Number(),
+          }),
+        }
+      )
+
+      // !   
+  );
+});
